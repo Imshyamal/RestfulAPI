8280d4a0b12a1f43a16d996862f7f4c4625d0062
imshyamal <shyamalkapatel@gmail.com>
2017-11-22 02:18:27 -0500

    Mongo connection

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 import express  from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import routes from './src/routes/crmRoutes';
 
 const app= express();
 const PORT = 3000;
+
+//creating mongoose connection
+mongoose.Promise=global.Promise;
+
+//replace local mongodb server url to actual one
+mongoose.connect('mongodb://localhost/CRMdb',{
+    useMongoClient:true
+});
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 
 routes(app)
 
